e20bfb6fd4aa007e2da81266ee7720381c10d17a
YinLei <1099129793@qq.com>
2020-12-07 17:42:17 +0800

    edit HTML structure

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# vue3-composition-todo-app-
-Vue3 Composition实现Todo应用
+# todo-app
+
+## Project setup
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn serve
+```
+
+### Compiles and minifies for production
+```
+yarn build
+```
+
+### Lints and fixes files
+```
+yarn lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <main>
+    <div class="container">
+      <h1>Welcome to use Vue3.0 Todo APP.</h1>
+      <div class="input-add">
+        <input type="text" name="todo"/>
+        <button>
+          <i class="plus"></i>
+        </button>
+      </div>
+      <div class="filters">
+        <span class="filter active">All</span>
+        <span class="filter">Undone</span>
+        <span class="filter">Done</span>
+      </div>
+      <div class="todo-list">
+        <div class="todo-item">
+          <label>
+            <input type="checkbox"/>
+            给王可尔说句我爱你
+            <span class="check-button"></span>
+          </label>
+        </div>
+        <div class="todo-item">
+          <label>
+            <input type="checkbox"/>
+            给王可尔说“祝你幸福”
+            <span class="check-button"></span>
+          </label>
+        </div>
+        <div class="todo-item">
+          <label>
+            <input type="checkbox"/>
+            给王可尔说“you complete me.”
+            <span class="check-button"></span>
+          </label>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
