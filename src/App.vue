aa4b2f615340689dc773f0c39ee96060c3bd12c2
YinLei <1099129793@qq.com>
2020-12-07 21:07:04 +0800

    edit css

--- src/App.vue
+++ src/App.vue
@@ -49,12 +49,125 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+main {
+  width: 100vw;
+  min-height: 100vh;
+  display: grid;
+  align-items: center;
+  justify-items: center;
+  background: rgb(203, 210, 240);
+}
+.container {
+  width: 60%;
+  max-width: 550px;
+  box-shadow: 0px 0px 24px rbga(0,0,0, .15);
+  border-radius: 24px;
+  padding: 48px 28px;
+  background-color: rgb(245, 246, 252);
+}
+h1 {
+  margin: 24px 0;
+  font-size: 28px;
+  color: #414873;
+}
+.input-add {
+  position: relative;
+  display: flex;
+  align-items: center;
+}
+.input-add input{
+  padding: 16px 52px 16px 18px;
+  border-radius: 48px;
+  border: none;
+  outline: none;
+  box-shadow: 0px 0px 24px rgba(0,0,0,.08);
+  width: 100%;
+  font-size: 16px;
+  color: #626262;
+}
+.input-add button{
+  width: 46px;
+  height: 46px;
+  border-radius: 50%;
+  background: linear-gradient(#c0a5f3, #7f95f7);
+  border: none;
+  outline: none;
+  color: #fff;
+  position: absolute;
+  right: 0px;
+  cursor: pointer;
+}
+.input-add .plus{
+  display: block;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(#fff,#fff), linear-gradient(#fff,#fff);
+  background-size: 50% 2px, 2px 50%;
+  background-position: center;
+  background-repeat: no-repeat;
+}
+.filters {
+  display: flex;
+  margin: 24px 2px;
+  color: #c0c2ce;
+  font-size: 14px;
+}
+.filters .filter{
+  margin-right: 14px;
+  transition: .8s;
+}
+.filters .filter.active{
+  color: #6b729c;
+  transform: scale(1.2);
+}
+.todo-list {
+  display: grid;
+  row-gap: 14px;
+}
+.todo-item {
+  background: #fff;
+  padding: 16px;
+  border-radius: 8px;
+  color: #626262;
+}
+.todo-item label{
+  position: relative;
+  display: flex;
+  align-items: center;
+}
+.todo-item label span.check-button{
+  position: absolute;
+  top: 0;
+}
+.todo-item label span.check-button::before,
+.todo-item label span.check-button::after{
+  content: "";
+  display: block;
+  position: absolute;
+  width: 18px;
+  height: 18px;
+  border-radius: 50%;
+}
+.todo-item label span.check-button::before{
+  border: 1px solid #b382f9;
+}
+.todo-item label span.check-button::after{
+  transition: .4s;
+  background: #b382f9;
+  transform: translate(1px, 1px) scale(.8);
+  opacity: 0;
+}
+.todo-item input {
+  margin-right: 16px;
+  opacity: 0;
+}
+.todo-item input:checked + span.check-button::after {
+  opacity: 1;
 }
 </style>
